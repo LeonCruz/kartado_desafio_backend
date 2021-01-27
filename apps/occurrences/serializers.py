2a031f92598765aa7a38f239f5e4109e96db1601
Leon Cruz <leon.cruz.teixeira@gmail.com>
2021-01-27 17:43:41 -0300

    Serializer para o model Occurrence

--- apps/occurrences/serializers.py
+++ apps/occurrences/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Status, Road, Occurrence
+
+from .models import Occurrence, Road, Status
 
 
 class StatusSerializer(serializers.HyperlinkedModelSerializer):
@@ -15,3 +16,25 @@ class RoadSerializer(serializers.HyperlinkedModelSerializer):
 
 
 # add new code below
+class OccurrenceSerizaliser(serializers.HyperlinkedModelSerializer):
+    road_name = serializers.SerializerMethodField("get_road_name")
+    status_name = serializers.SerializerMethodField("get_status_name")
+
+    def get_road_name(self, occurrence):
+        return occurrence.road.name
+
+    def get_status_name(self, occurrence):
+        return occurrence.status.name
+
+    class Meta:
+        model = Occurrence
+        fields = [
+            "description",
+            "road",
+            "road_name",
+            "km",
+            "status",
+            "status_name",
+            "created_at",
+            "updated_at",
+        ]
